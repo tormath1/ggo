a6ec8ff814103dd2cdbc7d9b99818c5f346b81c3
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-08 17:55:35 +0200

    perf(root): change tool name

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,19 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "gg",
-	Short: "GG is an implementation of GNU command in go",
+	Use:   "ggo",
+	Short: "GGO is an implementation of GNU command in go",
 	Long:  "Even sober we make mistakes",
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
