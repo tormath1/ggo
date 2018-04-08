be00096f16a23f4ef19b71046e7745fbc7bfd1d9
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-08 17:54:56 +0200

    feat(cp): implement simple copy command

--- cmd/cp.go
+++ cmd/cp.go
@@ -1,25 +1,59 @@
 package cmd
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
+var recursive bool
+
 func init() {
+	var cpCmd = &cobra.Command{
+		Use:   "cp <src> <destination>>",
+		Short: "cp <src> to <destination>",
+		Long:  `Do I need to write a description to copy a file`,
+		Args:  cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			main(args[0], args[1])
+		},
+	}
+
+	cpCmd.Flags().BoolVarP(&recursive, "recursive", "r", false, "copy a directory in a recursive way")
 	rootCmd.AddCommand(cpCmd)
 }
 
-var cpCmd = &cobra.Command{
-	Use:   "cp <src> <destination>>",
-	Short: "cp <src> to <destination>",
-	Long:  `Do I need to write a description to copy a file`,
-	Args:  cobra.MinimumNArgs(2),
-	Run: func(cmd *cobra.Command, args []string) {
-		cp(args[0], args[1])
-	},
+func main(src string, dst string) error {
+	if !recursive {
+		return cp(src, dst)
+	}
+	return cpRecursive(src, dst)
 }
 
-func cp(src string, dst string) {
-	fmt.Println(src, dst)
+func cp(src string, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return dstFile.Close()
+}
+
+func cpRecursive(src string, dst string) error {
+	log.Println("recursive")
+	return nil
 }
